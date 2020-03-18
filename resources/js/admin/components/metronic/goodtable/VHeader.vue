286723981995f62f98aa3983ec2c3e66d85f2968
slieman <slieman2001@hotmail.com>
2020-03-18 10:53:50 +0200

    start app.

---
+++ resources/js/admin/components/metronic/goodtable/VHeader.vue
@@ -0,0 +1,20 @@
+<template>
+ <div>
+ <button
+          class="btn btn-primary custom-btn"
+          data-toggle="modal"
+          data-target="#m_modal_5"
+        >{{ addbtn }}</button>
+ </div>
+      
+</template>
+
+<script>
+export default {
+  props:["addbtn"]
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/admin/views/Dashboard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row">
+    <div class="card ml-15" style="width: 18rem;">
+  <div class="card-body text-center">
+    <h5 class="card-title">Customers</h5>
+    <router-link to="/customers"><span class="fs-28">{{ customersCount }}</span></router-link>
+  </div>
+</div>
+ <div class="card ml-15" style="width: 18rem;">
+  <div class="card-body text-center">
+    <h5 class="card-title">Users</h5>
+    <router-link to="/users"><span class="fs-28">{{ usersCount }}</span></router-link>
+  </div>
+</div>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+export default {
+  data(){
+    return {
+      customersCount:0,
+      usersCount:0,
+    }
+  },
+  mounted(){
+    let customers = [];
+    axios.get('/api/customers').then(res=> {
+      customers = res.data.data
+        this.customersCount = customers.length
+    }),
+    axios.get('https://reqres.in/api/users/').then(res=> {
+      this.usersCount = res.data.data.length
+    });
+  }
+}
+</script>
+
+<style>
+.fs-28{
+  font-size: 28px;
+}
+.ml-15{
+  margin-left: 15px;
+}
+</style>
